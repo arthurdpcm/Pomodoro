62da88c9bfea6b3149a0f273a76171045c9466e8
arthurdpcm <duartearthur42@gmail.com>
2022-02-23 23:14:15 -0300

    first commit

---
+++ pomodoro/src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import './App.css'
+
+const COUNTDOWN_INITIAL_TIME = 25*60;
+
+function App() {
+  const [secondsAmount, setSecondsAmount] = useState(COUNTDOWN_INITIAL_TIME) // 25 minutes;
+  
+  // setSecondsAmount(COUNTDOWN_INITIAL_TIME)
+  useEffect(()=>{
+    if (secondsAmount === 0) {
+      alert("Chegou ao fim!")
+      return;
+    }
+
+    setTimeout(() => {
+      setSecondsAmount(secondsAmount - 1)
+    }, 1000)
+    
+  }, [secondsAmount]);
+
+  const handleReset = () => {
+    setSecondsAmount(COUNTDOWN_INITIAL_TIME);
+  }
+
+
+  const minutes = Math.floor(secondsAmount / 60);
+  const seconds = secondsAmount % 60;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <span>{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}</span>
+        <button className="button" onClick={()=>{handleReset()}}>
+          Reset
+        </button>
+        
+      </header>
+    </div>
+  )
+}
+
+export default App
