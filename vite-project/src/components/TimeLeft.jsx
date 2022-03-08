3ca1b5e4988287fa88b3c69f5eca706681f41a08
arthurdpcm <duartearthur42@gmail.com>
2022-03-07 21:18:37 -0300

    tailwind changes

--- vite-project/src/components/TimeLeft.jsx
+++ vite-project/src/components/TimeLeft.jsx
@@ -16,7 +16,8 @@ const TimeLeft = ({
     .format("mm:ss", { trim: false });
 
   return (
-    <div className="flex flex-col font-clock justify-evenly items-center w-64 h-64 bg-teal-300 rounded-full">
+    <div className="flex flex-col font-clock justify-evenly items-center w-64 h-64 bg-teal-300 rounded-full border-2
+    border-teal-900">
       <p className="text-teal-900 text-2xl" id="timer-label">
         {timerLabel}
       </p>
@@ -26,15 +27,16 @@ const TimeLeft = ({
 
       <div className="grid grid-flow-col gap-2">
         <button
-          className="text-teal-200 text-xl bg-teal-800 px-3 py-1  border-2
-          border-teal-900 border-solid rounded transition hover:bg-teal-200 hover:text-teal-900  duration-300 "
+          className="text-teal-200 text-1xl bg-teal-800 px-3 py-1  border-2
+          border-teal-900 border-solid rounded transition hover:bg-teal-200 hover:text-teal-900  duration-300"
           id="start_stop"
           onClick={handleStartStopClick}
         >
           {startStopButtonlabel}
         </button>
         <button
-          className="border-2 text-teal-900 rounded border-teal-900 border-solid px-3 py-1 transition hover:bg-teal-900 hover:text-teal-200 duration-300 "
+          className="text-teal-200 text-1xl bg-teal-800 px-3 py-1  border-2
+          border-teal-900 border-solid rounded transition hover:bg-teal-200 hover:text-teal-900  duration-300"
           id="reset"
           onClick={handleResetButtonClick}
         >
